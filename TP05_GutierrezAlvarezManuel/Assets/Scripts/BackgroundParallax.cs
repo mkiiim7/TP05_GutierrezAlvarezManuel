28ad3bb591129b06846637d378abb78ce5006b15
mkiiim7 <154470894+mkiiim7@users.noreply.github.com>
2024-10-28 20:41:25 -0300

    Cambios Parallax 1

--- TP05_GutierrezAlvarezManuel/Assets/Scripts/BackgroundParallax.cs
+++ TP05_GutierrezAlvarezManuel/Assets/Scripts/BackgroundParallax.cs
@@ -1,22 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BackgroundParallax : MonoBehaviour
 {
-                     private float startPos;
+    private float startPos;
+    private float length;
     [SerializeField] public GameObject cam;
     [SerializeField] public float parallaxSpeed;
 
     void Start()
     {
         startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
    
     void Update()
     {
         float distance = cam.transform.position.x * parallaxSpeed ;
+
+        float movement = cam.transform.position.x * (1- parallaxSpeed);
+
         transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (movement > startPos + length ) 
+        {
+            startPos += length;  
+        }
+        else if (movement < startPos - length)
+        {
+            startPos -= length;
+        }
     }
 }
